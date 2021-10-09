71716e6387c092d5ff57ca237defb9e06a356019
edersonlucas <edersonlucas@outlook.com.br>
2021-10-09 18:41:11 -0300

    Atualzacao do codigo

--- calculadoraIMC.py
+++ calculadoraIMC.py
@@ -1,23 +1,30 @@
 '''
- Recebe os dados
+Calculadora IMC
+Autor: Ederson Lucas
 '''
 
-ano_Atual = 2021
+
+#Entrada do Nome
+
 nome = input('Qual seu nome? ')
+
 # Laço ano
+
 while True:
-    ano_Nascimento = input('Em que ano você nasceu? ')
-    if ano_Nascimento.isdigit() == False or int(ano_Nascimento) >= 2022:
-        print('Digite um ano valido!')
+    idade = input('Qual sua idade? ')
+    if idade.isdigit() == False or int(idade) <= 0:
+        print('Digite uma idade valida!')
         continue
     else:
-        ano_Nascimento = int(ano_Nascimento)
-        if ano_Nascimento <= 1906:
+        idade = int(idade)
+        if idade >= 115:
             print('A Calculadora de IMC é somente para menores de 115 anos, porém receba nossos parabéns, com essa idade você é a pessoa mais velha do mundo.')
             continue
         else:
             break
+        
 # Laço altura
+
 while True:
     altura = input('Qual sua altura? ')
     if altura.isalnum() == True and altura.isdigit() == False or float(altura) <= 0.2:
@@ -30,7 +37,9 @@ while True:
             continue
         else:
             break
+        
 # Laço peso
+
 while True:
     peso = input('Qual seu peso? ')
     if peso.isalnum() == True and peso.isdigit() == False  or float(peso) <= 0.2:
@@ -40,20 +49,15 @@ while True:
         peso= float(peso)
         break
 imc = peso/(altura*altura)
-idade = ano_Atual - ano_Nascimento
 
-'''
- Imprime na tela
-'''
+#Imprime na tela
 
 print('-='*35)
-print('{}, nasceu em {}, tem {} anos de idade e seu IMC é {:.2f}'.format(nome, ano_Nascimento, idade, imc,));
+print('{}, tem {} anos de idade e seu IMC é {:.2f}'.format(nome, idade, imc,));
 print('-='*35)
 print('Status do seu IMC')
 
-'''
- Condições
-'''
+# Condições
 
 if imc < 16:
     print('Você está com Magreza Grave. Consulte um médico.');
@@ -72,4 +76,3 @@ elif imc >= 35 and imc < 40:
 elif imc >= 40:
     print('Você tem Obesidade Grau 3(Mórbida). Consulte um médico.');
 print('-='*35)
-
